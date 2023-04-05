60e1bb1e3be11ec3df9f9614886a12ca944bd024
yanglinhere <yanglinhere@icloud.com>
2023-04-05 15:47:57 +0800

    add

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 
-docker run --name dst -\
+args=$@
+
+docker run $args \
+--name dst -\
 -net=host \
--e http_proxy=10.0.4.6:7890 \
--e https_proxy=10.0.4.6:7890 \
 -v $(pwd)/dst_default_config/DoNotStarveTogether/Cluster_1:/data \
 jamesits/dst-server:latest
